103958e6b6982ee0f2f0469103d8ec63bf4825af
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-04 23:48:22 +0100

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,11 @@
+web4 --help
+web4 login
+web4 chat
+web4 ai prompt "Hello"
+web4 blockchain deploy
+web4 node start
+web4 update
+web4 doctor
+web4 config
+web4 plugins list
+web4 plugins install web4-security
