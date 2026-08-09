75534c2c497f8cf9f9ea3e260ffe04a998228776
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-09 04:50:03 +0100

    Update and rename bash.zsh to bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
-jlab --python-path /Users/username/custom_env/bin/python ../notebooks/test.ipynb
+jlab --python-path /etc/username/custom_env/bin/python ../notebooks/test.ipynb
 mkdir -p src/manager src/providers src/ai
 # Linux/macOS - remove the installation directory
 # Use the base environment path from 'mamba info' to find the exact location
@@ -9,7 +9,7 @@ sudo snap install code --classic
 # Also remove the package cache if it's in a separate location
 # (check the 'package cache' path from 'mamba info')
 rm -rf ~/.cache/conda/pkgs  # or your specific cache location
-touch src/index.ts
+touch src/index.d.ts
 touch src/manager/custom_contents.ts
 touch src/providers/local.ts
 touch src/providers/ipfs.ts
@@ -40,3 +40,14 @@ npm install -g @web4/cli
 
 # Docker
 docker run web4/cli
+
+docker run --rm -i -v "$PWD:$PWD" -w "$PWD" ghcr.io/jqlang/jq:latest '.version' package.json
+docker run --rm -i ghcr.io/jqlang/jq:latest < package.json '.version'
+git submodule update --init    # if building from git to get oniguruma
+autoreconf -i                  # if building from git
+./configure --with-oniguruma=builtin
+make clean                     # if upgrading from a version previously built from source
+make -j8
+make check
+sudo make install
+make LDFLAGS=-all-static
